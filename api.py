4fe02189218aca73a532af67b3c36e5ceae94cc9
Mats100 <muneeb75@icloud.com>
2023-01-17 19:46:44 +0500

    changed code

--- api.py
+++ api.py
@@ -7,65 +7,75 @@ from sqlalchemy.orm import Session
 
 models.Base.metadata.create_all(bind=engine)
 
+
 def get_db():
     try:
         db = SessionLocal()
         yield db
     finally:
         db.close()
+
+
 app = FastAPI()
 
+
 class Employee(BaseModel):
-    id: int
     name: str
     profession: str
+
 
 class UpdateData(BaseModel):
     id: Optional[int] = None
     name: Optional[str] = None
     profession: Optional[str] = None
 
-data = []
+
 
 @app.get("/")
 def read_api(db: Session = Depends(get_db)):
     return db.query(models.Employees).all()
 
-@app.get("/get-employee/{data_id}")
-def get_data(data_id: int = Path(None, description="The ID of the employee you would like to view", gt=0)):
-    return data[data_id]
+
+@app.get("/get-employee/")
+def get_data(employee: Employee = Path(None, description="The ID of the employee you would like to view", gt=0,)):
+    return employee
+
 
 @app.get("/get-by-name")
-def get_name(name: str = Query(None, title="Name", description="Name of employee")):
+def get_name(name: str = Query(None, title="Name", description="Name of employee"), db: Session = Depends(get_db),):
     for data_id in data:
-        if data[data_id].name == name:
-            return data[data_id]
+        if get_db() == name:
+            return db.query()
     return {"Data": "Not Found"}
 
+
 @app.post("/create-record/")
-def create_record(db: Session = Depends(get_db)):
-   employee_model = models.Employees()
-   db.add(employee_model)
-   db.commit()
-   db.refresh(employee_model)
+def create_record(employee: Employee, db: Session = Depends(get_db)):
+    employee_model = models.Employees(name=employee.name, profession=employee.profession)
+    db.add(employee_model)
+    db.commit()
+    db.refresh(employee_model)
+
 
 @app.put("/update-item/{data_id}")
-def update_item( employee: Employee, db: Session = Depends(get_db)):
-    Employee_model = db.query(models.Employees).filter(models.Employees.id == id).first()
-    if Employee_model is None:
+def update_item(employee: Employee, db: Session = Depends(get_db)):
+    employee_model = db.query(models.Employees).filter(models.Employees.id == id).first()
+    if employee_model is None:
         raise HTTPException(
             status_code=404,
-            detail=f"ID{}: Does not exist"
+            detail=f"ID{employee}: Does not exist"
         )
-    Employee_model.name = employee.name
-    Employee_model.profession =employee.profession
+    employee_model.name = employee.name
+    employee_model.profession = employee.profession
 
-    db.add(Employee_model)
+    db.add(employee_model)
     db.commit()
     return employee
+
+
 @app.delete("/{delete-id}")
-def delete_id(data_id: int = Query(..., description="The ID of the Employee you want to delete")):
-    if data_id not in data:
+def delete_id(employee: int = Query(..., description="The ID of the Employee you want to delete"), db: Session = Depends(get_db)):
+    if Employees.id not in get_db():
         return {"Error": "ID does not exist"}
-    del data[data_id]
+    del Employees.id
     return {"Success": "Item deleted Successfully"}
