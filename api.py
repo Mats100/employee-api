1be56a59064d03b8b29369fab324dcd7a5bcb672
Mats100 <muneeb75@icloud.com>
2023-01-18 20:11:25 +0500

    add code

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
 import models
 from database import engine, SessionLocal
@@ -18,9 +18,10 @@ def get_db():
 app = FastAPI()
 
 
-class Employee(BaseModel):
+class Employee_Data(BaseModel):
     name: str
     profession: str
+    experience: int
 
 
 @app.get("/")
@@ -29,22 +30,33 @@ def read_api(db: Session = Depends(get_db)):
 
 
 @app.post("/create-record/")
-def create_record(employee: Employee, db: Session = Depends(get_db)):
-    employee_model = models.Employees(name=employee.name, profession=employee.profession)
+def create_record(employee: Employee_Data, db: Session = Depends(get_db)):
+    employee_model = models.Employees(name=employee.name, profession=employee.profession, experience=employee.experience)
+
     db.add(employee_model)
     db.commit()
+    db.refresh(employee_model)
 
+    return employee_model
 
 @app.put("/update-user/{Id}")
-def update_user(Id: int, employee: Employee, db: Session = Depends(get_db)):
+def update_user(Id: int, employee: Employee_Data, db: Session = Depends(get_db)):
     employee_model = db.query(models.Employees).filter(models.Employees.id == Id).first()
     if employee_model is None:
         raise HTTPException(
-            status_code=404,
+            status_code=404, detail="User not Found"
         )
     employee_model.name = employee.name
     employee_model.profession = employee.profession
-
     db.commit()
     db.refresh(employee_model)
     return employee_model
+
+@app.delete("/delete-user/{Id}")
+def delete_user(Id: int, db: Session = Depends(get_db)):
+    employee_model = db.query(models.Employees).filter(models.Employees.id == Id).first()
+    if employee_model is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    db.delete(employee_model)
+    db.commit()
+    return {"message": "User deleted Successfully"}
--- models.py
+++ models.py
@@ -7,5 +7,6 @@ class Employees(Base):
     id = Column(INTEGER, primary_key=True, index=True)
     name = Column(String)
     profession = Column(String)
+    experience = Column(INTEGER, index=True)
 
 
